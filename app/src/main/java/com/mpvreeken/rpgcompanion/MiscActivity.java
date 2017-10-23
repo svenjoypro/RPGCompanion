21fddf8f3cdeb00d81759baf57f91000847ebe50
Michael Vreeken <svenjoypro@gmail.com>
2017-10-23 10:35:02 -0700

    Add 16 new d100 lists (and the custom theme for the Nougat NPC save dialog fix)

--- app/src/main/java/com/mpvreeken/rpgcompanion/MiscActivity.java
+++ app/src/main/java/com/mpvreeken/rpgcompanion/MiscActivity.java
@@ -1,12 +1,13 @@
 package com.mpvreeken.rpgcompanion;
 
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -25,13 +26,18 @@ import java.util.Random;
 public class MiscActivity extends AppCompatActivity {
 
     private JSONArray tavern_adjs, tavern_nouns, long_rests, road_encounters, romantic_love_interests, slums_encounters, strange_encounters, trail_encounters, backpack_contents_tools, backpack_contents_edibles, backpack_contents_personals, backpack_contents_strange, book_titles, necromancers_lair, curious_items, kitchen_items, magic_umbrellas, minor_magic_items, mundane_treasures, potion_colors, potion_scents, potion_appearances, potion_effects, ruined_building_contents, weapon_names, tavern_drinks, tavern_foods, deep_dark_woods, trinkets, magic_skulls, town_names, curses, flawed_utopias, lone_graves;
+    private JSONArray good_smells, witches, monster_deaths, blacksmith_wares, suspicious_village_encounters, nice_village_encounters, jungle_encounters, misc_items, antagonist_bios, necromantic_effects, unique_horses, rich_corpses, zombie_variations, unique_spellbooks, gossip, plants_trees;
     private TextView output_tv;
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_misc);
+
+        //Set up back button to appear in action bar
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.setHomeButtonEnabled(true);
+        actionBar.setDisplayHomeAsUpEnabled(true);
 
         output_tv = findViewById(R.id.misc_output_tv);
 
@@ -41,6 +47,10 @@ public class MiscActivity extends AppCompatActivity {
         Button se_btn = findViewById(R.id.misc_slums_encounters_btn);
         Button sfe_btn = findViewById(R.id.misc_strange_encounters_btn);
         Button te_btn = findViewById(R.id.misc_trail_encounters_btn);
+        Button sve_btn = findViewById(R.id.misc_suspicious_village_encounters_btn);
+        Button nve_btn = findViewById(R.id.misc_nice_village_encounters_btn);
+        Button je_btn = findViewById(R.id.misc_jungle_encounters_btn);
+
         Button bc_btn = findViewById(R.id.misc_backpack_contents_btn);
         Button bt_btn = findViewById(R.id.misc_book_titles_btn);
         Button tnl_btn = findViewById(R.id.misc_creepy_necromancers_lair_btn);
@@ -56,12 +66,31 @@ public class MiscActivity extends AppCompatActivity {
         Button tddw_btn = findViewById(R.id.misc_things_in_the_woods_btn);
         Button t_btn = findViewById(R.id.misc_trinkets_btn);
         Button ms_btn = findViewById(R.id.misc_magic_skulls_btn);
+        Button bw_btn = findViewById(R.id.misc_blacksmith_wares_btn);
+        Button mi_btn = findViewById(R.id.misc_misc_items_btn);
+        Button rc_btn = findViewById(R.id.misc_rich_corpses_btn);
+        Button sb_btn = findViewById(R.id.misc_unique_spellbooks_btn);
+        Button pt_btn = findViewById(R.id.misc_plants_trees_btn);
+
         Button tn_btn = findViewById(R.id.misc_town_names_btn);
         Button tavern_btn = findViewById(R.id.misc_tavern_name_btn);
         Button wn_btn = findViewById(R.id.misc_weapon_names_btn);
+
+        Button w_btn = findViewById(R.id.misc_witches_btn);
+        Button ab_btn = findViewById(R.id.misc_antagonist_bios_btn);
+        Button uh_btn = findViewById(R.id.misc_unique_horses_btn);
+        Button zv_btn = findViewById(R.id.misc_zombie_variations_btn);
+
+
         Button c_btn = findViewById(R.id.misc_curses_btn);
         Button fu_btn = findViewById(R.id.misc_flawed_utopias_btn);
         Button slg_btn = findViewById(R.id.misc_lone_graves_btn);
+        Button gs_btn = findViewById(R.id.misc_good_smells_btn);
+        Button md_btn = findViewById(R.id.misc_monster_deaths_btn);
+        Button ne_btn = findViewById(R.id.misc_necromantic_effects_btn);
+        Button g_btn = findViewById(R.id.misc_gossip_btn);
+
+
 
         View.OnClickListener buttonHandler = new View.OnClickListener() {
             public void onClick(View v) {
@@ -187,6 +216,104 @@ public class MiscActivity extends AppCompatActivity {
                         }
                         showOutput(generateTavernName());
                         break;
+
+
+                    case R.id.misc_good_smells_btn:
+                        if (good_smells == null) {
+                            good_smells = loadJSON(R.raw.d100_good_smells, "good_smells");
+                        }
+                        showOutput(getRandomBasic(good_smells));
+                        break;
+                    case R.id.misc_witches_btn:
+                        if (witches == null) {
+                            witches = loadJSON(R.raw.d100_witches, "witches");
+                        }
+                        showOutput(getRandomBasic(witches));
+                        break;
+                    case R.id.misc_monster_deaths_btn:
+                        if (monster_deaths == null) {
+                            monster_deaths = loadJSON(R.raw.d100_monster_deaths, "monster_deaths");
+                        }
+                        showOutput(getRandomBasic(monster_deaths));
+                        break;
+                    case R.id.misc_blacksmith_wares_btn:
+                        if (blacksmith_wares == null) {
+                            blacksmith_wares = loadJSON(R.raw.d100_blacksmith_wares, "blacksmith_wares");
+                        }
+                        showOutput(getRandomBasic(blacksmith_wares));
+                        break;
+                    case R.id.misc_suspicious_village_encounters_btn:
+                        if (suspicious_village_encounters == null) {
+                            suspicious_village_encounters = loadJSON(R.raw.d100_suspicious_village_encounters, "suspicious_village_encounters");
+                        }
+                        showOutput(getRandomBasic(suspicious_village_encounters));
+                        break;
+                    case R.id.misc_nice_village_encounters_btn:
+                        if (nice_village_encounters == null) {
+                            nice_village_encounters = loadJSON(R.raw.d100_nice_village_encounters, "nice_village_encounters");
+                        }
+                        showOutput(getRandomBasic(nice_village_encounters));
+                        break;
+                    case R.id.misc_jungle_encounters_btn:
+                        if (jungle_encounters == null) {
+                            jungle_encounters = loadJSON(R.raw.d100_jungle_encounters, "jungle_encounters");
+                        }
+                        showOutput(getRandomBasic(jungle_encounters));
+                        break;
+                    case R.id.misc_misc_items_btn:
+                        if (misc_items == null) {
+                            misc_items = loadJSON(R.raw.d100_misc_items, "misc_items");
+                        }
+                        showOutput(getRandomBasic(misc_items));
+                        break;
+                    case R.id.misc_antagonist_bios_btn:
+                        if (antagonist_bios == null) {
+                            antagonist_bios = loadJSON(R.raw.d100_antagonist_bios, "antagonist_bios");
+                        }
+                        showOutput(getRandomBasic(antagonist_bios));
+                        break;
+                    case R.id.misc_necromantic_effects_btn:
+                        if (necromantic_effects == null) {
+                            necromantic_effects = loadJSON(R.raw.d100_necromantic_effects, "necromantic_effects");
+                        }
+                        showOutput("Allows caster to: "+getRandomBasic(necromantic_effects));
+                        break;
+                    case R.id.misc_unique_horses_btn:
+                        if (unique_horses == null) {
+                            unique_horses = loadJSON(R.raw.d100_unique_horses, "unique_horses");
+                        }
+                        showOutput(getRandomBasic(unique_horses));
+                        break;
+                    case R.id.misc_rich_corpses_btn:
+                        if (rich_corpses == null) {
+                            rich_corpses = loadJSON(R.raw.d100_rich_corpses, "rich_corpses");
+                        }
+                        showOutput(getRandomBasic(rich_corpses));
+                        break;
+                    case R.id.misc_zombie_variations_btn:
+                        if (zombie_variations == null) {
+                            zombie_variations = loadJSON(R.raw.d100_zombie_variations, "zombie_variations");
+                        }
+                        showOutput(getRandomBasic(zombie_variations));
+                        break;
+                    case R.id.misc_unique_spellbooks_btn:
+                        if (unique_spellbooks == null) {
+                            unique_spellbooks = loadJSON(R.raw.d100_unique_spellbooks, "unique_spellbooks");
+                        }
+                        showOutput(getRandomBasic(unique_spellbooks));
+                        break;
+                    case R.id.misc_plants_trees_btn:
+                        if (plants_trees == null) {
+                            plants_trees = loadJSON(R.raw.d100_plants_trees, "plants_trees");
+                        }
+                        showOutput(getRandomBasic(plants_trees));
+                        break;
+                    case R.id.misc_gossip_btn:
+                        if (gossip == null) {
+                            gossip = loadJSON(R.raw.d100_gossip, "gossip");
+                        }
+                        showOutput(getRandomBasic(gossip));
+                        break;
                     default:
 
                 }
@@ -220,6 +347,23 @@ public class MiscActivity extends AppCompatActivity {
         c_btn.setOnClickListener(buttonHandler);
         fu_btn.setOnClickListener(buttonHandler);
         slg_btn.setOnClickListener(buttonHandler);
+
+        sve_btn.setOnClickListener(buttonHandler);
+        nve_btn.setOnClickListener(buttonHandler);
+        je_btn.setOnClickListener(buttonHandler);
+        bw_btn.setOnClickListener(buttonHandler);
+        mi_btn.setOnClickListener(buttonHandler);
+        rc_btn.setOnClickListener(buttonHandler);
+        sb_btn.setOnClickListener(buttonHandler);
+        pt_btn.setOnClickListener(buttonHandler);
+        w_btn.setOnClickListener(buttonHandler);
+        ab_btn.setOnClickListener(buttonHandler);
+        uh_btn.setOnClickListener(buttonHandler);
+        zv_btn.setOnClickListener(buttonHandler);
+        gs_btn.setOnClickListener(buttonHandler);
+        md_btn.setOnClickListener(buttonHandler);
+        ne_btn.setOnClickListener(buttonHandler);
+        g_btn.setOnClickListener(buttonHandler);
 
     }
 
@@ -342,5 +486,18 @@ public class MiscActivity extends AppCompatActivity {
             e.printStackTrace();
         }
         return s;
+    }
+
+
+    //Set click listener for back button in action bar
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                this.finish();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 }
