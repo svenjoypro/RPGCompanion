e2a7360078fecb66cb27510a8985cfcee07f9e6c
Michael Vreeken <svenjoypro@gmail.com>
2017-10-24 17:47:32 -0700

    Add a textview to the main screen that retrieves text from the website for updates (without having to update the app)

--- app/src/main/java/com/mpvreeken/rpgcompanion/MainActivity.java
+++ app/src/main/java/com/mpvreeken/rpgcompanion/MainActivity.java
@@ -5,6 +5,18 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageButton;
+import android.widget.TextView;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
+
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
 
 public class MainActivity extends AppCompatActivity {
 	
@@ -14,6 +26,7 @@ public class MainActivity extends AppCompatActivity {
 	 *
 	 *
 	 */
+	private TextView webalert_tv;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,13 +35,15 @@ public class MainActivity extends AppCompatActivity {
 
         //checkLoggedIn();
 
-        ImageButton hooks_btn = (ImageButton) findViewById(R.id.main_hooks_button);
-        ImageButton encounters_btn = (ImageButton) findViewById(R.id.main_encounters_button);
-        ImageButton puzzles_btn = (ImageButton) findViewById(R.id.main_puzzles_button);
-        ImageButton npc_btn = (ImageButton) findViewById(R.id.main_npc_button);
-        ImageButton adventures_btn = (ImageButton) findViewById(R.id.main_adventures_button);
-        ImageButton riddles_btn = (ImageButton) findViewById(R.id.main_riddles_button);
-        ImageButton misc_btn = (ImageButton) findViewById(R.id.main_misc_button);
+        ImageButton hooks_btn = findViewById(R.id.main_hooks_button);
+        ImageButton encounters_btn = findViewById(R.id.main_encounters_button);
+        ImageButton puzzles_btn = findViewById(R.id.main_puzzles_button);
+        ImageButton npc_btn = findViewById(R.id.main_npc_button);
+        ImageButton adventures_btn = findViewById(R.id.main_adventures_button);
+        ImageButton riddles_btn = findViewById(R.id.main_riddles_button);
+        ImageButton misc_btn = findViewById(R.id.main_misc_button);
+
+        webalert_tv = findViewById(R.id.main_web_alert_tv);
 
 
         View.OnClickListener buttonHandler = new View.OnClickListener() {
@@ -72,6 +87,47 @@ public class MainActivity extends AppCompatActivity {
         adventures_btn.setOnClickListener(buttonHandler);
         misc_btn.setOnClickListener(buttonHandler);
 
+        getWebAlert();
+    }
 
+    private void getWebAlert() {
+        String hooks_url = getResources().getString(R.string.url_get_webalert);
+
+        OkHttpClient client = new OkHttpClient();
+
+        Request request = new Request.Builder()
+                .url(hooks_url)
+                .build();
+
+        client.newCall(request).enqueue(new Callback() {
+            @Override
+            public void onFailure(Call call, IOException e) { e.printStackTrace(); }
+
+            @Override
+            public void onResponse(Call call, final Response response) throws IOException {
+                if (!response.isSuccessful()) { throw new IOException("Unexpected code " + response); }
+                else {
+                    JSONObject r = null;
+                    String o = "";
+                    try {
+                        r = new JSONObject(response.body().string());
+                        o = r.getString("msg");
+                    } catch (JSONException e) {
+                        e.printStackTrace();
+                    }
+                    if (o.length()>0) {
+                        //We can't update the UI on a background thread, so run on the UI thread
+                        final String output = o;
+                        MainActivity.this.runOnUiThread(new Runnable() {
+                            @Override
+                            public void run() {
+                                webalert_tv.setText(output);
+                                webalert_tv.setVisibility(View.VISIBLE);
+                            }
+                        });
+                    }
+                }
+            }
+        });
     }
 }
