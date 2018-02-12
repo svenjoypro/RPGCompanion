4ec21b6364d5c904d760905f951c8e1eefcfed47
Michael Vreeken <svenjoypro@gmail.com>
2018-02-11 20:00:33 -0800

    Updates to account and settings pages. Account now shows more stats, settings allows external link alert toggle

--- app/src/main/java/com/mpvreeken/rpgcompanion/AccountActivity.java
+++ app/src/main/java/com/mpvreeken/rpgcompanion/AccountActivity.java
@@ -15,12 +15,15 @@ import okhttp3.OkHttpClient;
 import okhttp3.Request;
 import okhttp3.Response;
 
-public class AccountActivity extends RPGCActivity {
+public class AccountActivity extends RPGCAuthActivity {
 
-    private TextView username_tv, email_tv, created_at_tv;
+    private TextView username_tv, email_tv, created_at_tv, total_upvotes_tv, total_downvotes_tv;
     private TextView maps_tv, map_ups_tv, map_downs_tv;
     private TextView riddles_tv, riddle_ups_tv, riddle_downs_tv;
     private TextView hooks_tv, hook_ups_tv, hook_downs_tv;
+    private TextView items_tv, item_ups_tv, item_downs_tv;
+    private TextView puzzles_tv, puzzle_ups_tv, puzzle_downs_tv;
+    private int upvotes, downvotes;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -40,6 +43,8 @@ public class AccountActivity extends RPGCActivity {
         username_tv=findViewById(R.id.account_username_tv);
         email_tv=findViewById(R.id.account_email_tv);
         created_at_tv=findViewById(R.id.account_created_at_tv);
+        total_upvotes_tv=findViewById(R.id.account_total_upvotes_tv);
+        total_downvotes_tv=findViewById(R.id.account_total_downvotes_tv);
         maps_tv=findViewById(R.id.account_maps_tv);
         map_ups_tv=findViewById(R.id.account_map_ups_tv);
         map_downs_tv=findViewById(R.id.account_map_downs_tv);
@@ -49,6 +54,12 @@ public class AccountActivity extends RPGCActivity {
         hooks_tv=findViewById(R.id.account_hooks_tv);
         hook_ups_tv=findViewById(R.id.account_hook_ups_tv);
         hook_downs_tv=findViewById(R.id.account_hook_downs_tv);
+        items_tv=findViewById(R.id.account_items_tv);
+        item_ups_tv=findViewById(R.id.account_item_ups_tv);
+        item_downs_tv=findViewById(R.id.account_item_downs_tv);
+        puzzles_tv=findViewById(R.id.account_puzzles_tv);
+        puzzle_ups_tv=findViewById(R.id.account_puzzle_ups_tv);
+        puzzle_downs_tv=findViewById(R.id.account_puzzle_downs_tv);
     }
 
     private void getAccountInfo() {
@@ -106,6 +117,17 @@ public class AccountActivity extends RPGCActivity {
             hooks_tv.setText("Number of Plot Hooks submitted: "+r.getString("hooks"));
             hook_ups_tv.setText("Total Plot Hook upvotes: "+r.getString("hook_ups"));
             hook_downs_tv.setText("Total Plot Hook downvotes: "+r.getString("hook_downs"));
+            items_tv.setText("Number of Unique Items submitted: "+r.getString("items"));
+            item_ups_tv.setText("Total Unique Item upvotes: "+r.getString("item_ups"));
+            item_downs_tv.setText("Total Unique Item downvotes: "+r.getString("item_downs"));
+            puzzles_tv.setText("Number of Puzzles submitted: "+r.getString("puzzles"));
+            puzzle_ups_tv.setText("Total Puzzle upvotes: "+r.getString("puzzle_ups"));
+            puzzle_downs_tv.setText("Total Puzzle downvotes: "+r.getString("puzzle_downs"));
+
+            upvotes = r.getInt("map_ups") + r.getInt("riddle_ups") + r.getInt("puzzle_ups") + r.getInt("hook_ups") + r.getInt("item_ups");
+            downvotes = r.getInt("map_downs") + r.getInt("riddle_downs") + r.getInt("puzzle_downs") + r.getInt("hook_downs") + r.getInt("item_downs");
+            total_upvotes_tv.setText("Total Upvotes: " + String.valueOf(upvotes));
+            total_downvotes_tv.setText("Total Downvotes: " + String.valueOf(downvotes));
         } catch (JSONException e) {
             Log.d("RPGCApplication", e.getMessage());
             onHttpResponseError("An unknown error has occurred. Please try again.");
--- app/src/main/java/com/mpvreeken/rpgcompanion/SettingsActivity.java
+++ app/src/main/java/com/mpvreeken/rpgcompanion/SettingsActivity.java
@@ -1,16 +1,25 @@
 package com.mpvreeken.rpgcompanion;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.CompoundButton;
+import android.widget.Switch;
 
-public class SettingsActivity extends AppCompatActivity {
-
-    //TODO
-    //Setting to ask/always accept visiting external links
+public class SettingsActivity extends RPGCActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
+
+        Switch ela_switch = findViewById(R.id.settings_external_link_alert_switch);
+
+        ela_switch.setChecked(application.getExternalLinkAlert());
+
+        ela_switch.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
+                application.setExternalLinkAlert(b);
+            }
+        });
     }
 }
